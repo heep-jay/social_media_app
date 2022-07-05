f0886e9a74b4ffadcd186724883b7b57247aaf1a
heep-jay <jubrilmuritala69@gmail.com>
2022-07-05 09:02:41 +0800

    fifth commit (fixed authentication issue)

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Routes, Route, useNavigate} from "react-router-dom";
 import Login from './componenets/Login';
 import Home from './container/Home';
@@ -6,6 +6,16 @@ import Home from './container/Home';
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 const App = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    if (!User) navigate('/login');
+  }, []);
+
+
   return (
     <Routes>
       <Route path='login' element={<Login/>}/>
